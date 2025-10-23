2a6602f623be46a85645eae0d422b3ca98dc9c1b
Bilal <bilalbarina1@gmail.com>
2025-10-23 17:41:53 +0100

    fix: navigation for mobile

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -43,24 +43,32 @@ $(function () {
     window.addEventListener("scroll", navHighlighter);
 
     function navHighlighter() {
+        try {
+            $(".btn-close").click();
+        } catch (e) {
+            // Do nothing
+        }
 
-        let scrollY = window.pageYOffset;
+        try {
+            let scrollY = window.pageYOffset;
 
-        sections.forEach(current => {
-            const sectionHeight = current.offsetHeight;
-            const sectionTop = current.offsetTop - 100;
-            sectionId = current.getAttribute("id");
+            sections.forEach(current => {
+                const sectionHeight = current.offsetHeight;
+                const sectionTop = current.offsetTop - 100;
+                sectionId = current.getAttribute("id");
 
-            if (
-                scrollY > sectionTop &&
-                scrollY <= sectionTop + sectionHeight
-            ) {
-                document.querySelector(".navbar-collapse a[href*=" + sectionId + "]").classList.add("active");
-            } else {
-                document.querySelector(".navbar-collapse a[href*=" + sectionId + "]").classList.remove("active");
-            }
-        });
+                if (
+                    scrollY > sectionTop &&
+                    scrollY <= sectionTop + sectionHeight
+                ) {
+                    document.querySelector(".navbar-collapse a[href*=" + sectionId + "]").classList.add("active");
+                } else {
+                    document.querySelector(".navbar-collapse a[href*=" + sectionId + "]").classList.remove("active");
+                }
+            });
+        } catch (e) {
+            // Do nothing
+        }
     }
-
 });
 
